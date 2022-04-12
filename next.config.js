badd5300716d948aceb0b0c15df5b7547951ccf6
Jerônimo Jairo <jeronimo.araujo@ccc.ufcg.edu.br>
2022-04-12 18:29:59 -0300

    Make Image capable to use srcs from other domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  images: {
+    domains : ['storage.googleapis.com']
+  },
   webpack: (config, {isServer}) => {
     if (!isServer) config.resolve.fallback.fs = false;
     return config
